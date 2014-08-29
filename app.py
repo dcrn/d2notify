4325c884489e5f30409781f7e96a9548b1cceb57
Declan Curran <entityingredient@gmail.com>
2014-08-29 19:04:43 +0100

    Finished /poll view and caching

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, g, jsonify, render_template
 import json, urllib.request, json, time, os
 
 app = Flask(__name__)
+apikey = ''
 poll_time = 120
 last_poll = 0
 items_hash = ''
@@ -9,22 +10,32 @@ items_hash = ''
 @app.before_first_request
 def before_first_request(*args, **kwargs):
 	# Get Steam API key
+	global apikey
 	apifile = open('apikey.txt', 'r')
-	g.apikey = apifile.read()
+	apikey = apifile.read()
 
 @app.route('/poll')
 def poll():
-	return g.apikey
+	global apikey, last_poll, poll_time, items_hash
+
 	if time.time() > (last_poll + poll_time):
-		# Get items file hash
 		last_poll = time.time()
-		return ''
-	else:
-		return items_hash
+		jdata = ''
+		try:
+			jdata = urllib.request.urlopen('http://api.steampowered.com/IEconItems_570/GetSchemaURL/v1?key=' + apikey).read()
+		except:
+			return ''
+
+		j = json.loads(str(jdata, 'UTF-8'))
+		items_hash = j['result']['items_game_url'][-44:-4]
+	
+	return items_hash
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+	global items_hash
+
+	return render_template('index.html', hash=items_hash)
 
 def main():
 	if not os.path.isfile('apikey.txt'):
